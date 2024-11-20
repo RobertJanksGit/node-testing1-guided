5af4e43dd6a18e2cae461853d8d2730b971e5aba
Robert Jank <robert.jank@yahoo.com>
2024-11-20 08:42:25 -0600

    wip

--- car/car.spec.js
+++ car/car.spec.js
@@ -1,1 +1,44 @@
+const Car = require("./car");
+
+function foo() {
+  return "the foo";
+}
+
 // Test away!
+describe("our first tests", () => {
+  test("sanity", () => {
+    expect(5).toBe(5);
+    expect(5).toBe(3 + 2);
+    expect(5).toBeDefined();
+  });
+  test("objects", () => {
+    const o = { a: 1 };
+    const oo = { a: 1 };
+    const ooo = oo;
+    expect(o).toBe(o);
+    expect(oo).toBe(ooo);
+  });
+  test("objects again", () => {
+    expect({ a: 1 }).toEqual({ a: 1 });
+  });
+  describe("foo function", () => {
+    test("foo returns the foo", () => {
+      expect(foo()).toBe("the foo");
+      expect(foo()).toHaveLength(7);
+    });
+  });
+});
+describe("Car class", () => {
+  test("it is defined", () => {
+    expect(Car).toBeDefined();
+    expect(Car).toBeInstanceOf(Function);
+  });
+  test("has model and make", () => {
+    const prius = new Car("toyota", "prius");
+    expect(prius).toHaveProperty("make");
+    expect(prius).toHaveProperty("model");
+    expect(prius.make).toBeDefined();
+    expect(prius.model).toBeDefined();
+    expect(prius).toEqual({ make: "toyota", model: "prius" });
+  });
+});
